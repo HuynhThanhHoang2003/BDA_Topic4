a2bcdfe1134a9117b2239208c448d955cbf94a6b
HuynhThanhHoang2003 <khaonienhoang2003@gmail.com>
2025-08-28 17:47:03 +0700

    lan2

--- entities/Employee.java
+++ entities/Employee.java
@@ -1,4 +1,71 @@
 package sales.entities;
 
 public class Employee {
+    private int id;
+    private String lastName;
+    private String firstName;
+    private String birthName;
+    private int supervisor;
+
+    public Employee(){}
+
+    public Employee(int id, String lastName, String firstName, String birthName, int supervisor) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.birthName = birthName;
+        this.supervisor = supervisor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getBirthName() {
+        return birthName;
+    }
+
+    public void setBirthName(String birthName) {
+        this.birthName = birthName;
+    }
+
+    public int getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(int supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "ID: %-6d | Last Name: %-15s | First Name: %-15s | Birth Name: %-15s | Supervisor: %-6d",
+                id,
+                lastName,
+                firstName,
+                birthName,
+                supervisor
+        );
+    }
 }
--- entities/Order.java
+++ entities/Order.java
@@ -1,4 +1,60 @@
 package sales.entities;
 
 public class Order {
+    private int id;
+    private int customerId;
+    private int employeeId;
+    private String orderDate;
+
+    public Order(){}
+
+    public Order(String orderDate, int employeeId, int customerId, int id) {
+        this.orderDate = orderDate;
+        this.employeeId = employeeId;
+        this.customerId = customerId;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Order ID: %-6d | Customer ID: %-6d | Employee ID: %-6d | Order Date: %-12s",
+                id,
+                customerId,
+                employeeId,
+                orderDate
+        );
+    }
 }
